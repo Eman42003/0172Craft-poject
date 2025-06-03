6cd718ebedf89e1d8dbebcf40a50777cce1797cb
Eman Ashraf <eman65540@gmail.com>
2025-06-03 17:55:01 +0300

    config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-// إعدادات الاتصال بقاعدة البيانات
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "graduation_project";  // اسم قاعدة البيانات
+$dbname = "graduation_project";  
 
-// إنشاء الاتصال
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// التحقق من الاتصال
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
